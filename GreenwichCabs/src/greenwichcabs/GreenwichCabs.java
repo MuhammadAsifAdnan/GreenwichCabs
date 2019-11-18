9488babddafe9824454bb07c9e1f3fda5c19ef76
Asif <asif.adnan@ixorasolution.com>
2019-11-18 12:49:44 +0600

    added driver creation method

--- GreenwichCabs/src/greenwichcabs/GreenwichCabs.java
+++ GreenwichCabs/src/greenwichcabs/GreenwichCabs.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package greenwichcabs;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.*;
 
 /**
@@ -23,6 +25,24 @@ public class GreenwichCabs {
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // This makes sure when closing the window the app is
         // terminated. As we are using custom window close button, this actually wont do anything.
         mainFrame.setVisible(true);
+        
+        createDriverList();
+    }
+    
+    private static void createDriverList() {
+        List<Driver> drivers = new ArrayList<Driver>();
+        drivers.add(new Driver(1, "John Doe"));
+        drivers.add(new Driver(2, "Jack Sparrow"));
+        drivers.add(new Driver(3, "Hans Down"));
+        drivers.add(new Driver(4, "Alan Fresco"));
+        drivers.add(new Driver(5, "Malcom Function"));
+        drivers.add(new Driver(6, "Sue Shei"));
+        drivers.add(new Driver(7, "Richard Tea"));
+        drivers.add(new Driver(8, "Theodore Handle"));
+        drivers.add(new Driver(9, "Elenor Fant"));
+        drivers.add(new Driver(10, "Archibald Northbottom"));
+        
+        
     }
     
 }
